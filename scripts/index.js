e4a6b3b2b6938d690f8f3b133d1e038eacf26cba
Jonathan Britton <jbritttt29@hotmail.com>
2022-08-16 14:19:55 +0100

    form functionality complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,47 @@
-const modal = document.querySelector(".stats-modal")
+const subBtn = document.getElementById("sub-btn")
 const span = document.querySelector("span")
-const water = document.querySelector(".water")
+
 const modalBackdrop = document.querySelector(".habit-modal-backdrop")
 const habitModal = document.querySelector(".habit-modal")
 const closeModal = document.querySelector(".close")
+
+
+
+const form = document.querySelector('form')
+var elementToRename = document.getElementById("form-key");
+var elementToRenam = document.getElementById("label-text");
+
+
+
+form.addEventListener('submit', function (e){
+console.log('hello')
+const box = document.querySelectorAll(".check_box")
+    box.forEach(b => b.checked = false)
+    e.preventDefault()
+    const prePayload = new FormData(form);
+    const payload = new URLSearchParams(prePayload);
+
+    console.log([...payload])
+
+    fetch('http://localhost:3000', {
+
+method: 'POST',
+body:payload,
+})
+
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .then(err => console.log(err))
+
+})
+
+
+subBtn.addEventListener('click', ()=>{
+    
+    modalBackdrop.style.left = '-400px'
+   
+})
+
 
 
 closeModal.addEventListener('click', ()=>{
@@ -14,13 +52,47 @@ closeModal.addEventListener('click', ()=>{
 
 
 
+function modalHandler(){
+    modalBackdrop.style.display = 'block'
+    modalBackdrop.style.left = '0px'
 
+}
 
-water.addEventListener('click', ()=>{
+document.addEventListener('click', (e)=>{
     
-   
-        modalBackdrop.style.display = 'block'
-        modalBackdrop.style.left = '0px'
+  
+
+    if(e.target.id == 'water'){
+        modalHandler()
+        elementToRename.setAttribute("name","Drink Water");
+        elementToRenam.textContent = 'Drink 2ltr Water'
+        
+    } else if(e.target.id == 'exercise'){
+        modalHandler()
+        elementToRename.setAttribute("name","Exercise");
+        elementToRenam.textContent = 'Exercise'
+        
+    } else if(e.target.id == 'sleep'){
+        modalHandler()
+        elementToRename.setAttribute("name","Sleep");
+        elementToRenam.textContent = 'Sleep 8 hours'
+        
+    }else if(e.target.id == 'read'){
+        modalHandler()
+        elementToRename.setAttribute("name","Read");
+        elementToRenam.textContent = 'Read'
+        
+    }else if(e.target.id == 'gaming'){
+        modalHandler()
+        elementToRename.setAttribute("name","Gaming");
+        elementToRenam.textContent = 'Gaming'
+        
+    }else if(e.target.id == 'study'){
+        modalHandler()
+        elementToRename.setAttribute("name","Study");
+        elementToRenam.textContent = 'Study'
+        
+    }else;
 })
     
 
