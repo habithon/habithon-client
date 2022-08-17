cf6fb0b0dd88c347c0d021285701558c4ab81007
edisemin <edisemin@hotmail.com>
2022-08-17 16:38:19 +0100

    Adding buttons dynamically to daily goals page

--- scripts/daily-goals.js
+++ scripts/daily-goals.js
@@ -12,16 +12,20 @@ logout();
         const div = document.createElement('div')
         div.classList.add("goals-item")
         div.innerHTML = ` 
-        <img src="images/goals-water.jpg" alt="">
+        <div class="goals-image"> <img src="images/daily-goals-${habit.habit}.jpg" alt=""></div>
+        <div class="goals-item-extra"></div>
         <div class='stats-wrapper'> <h2>${habit.habit}</h2>
             <p><strong>Frequency: </strong>  <span>${habit.frequency}</span></p>
             <p><strong>Current streak: </strong>  <span>${habit.streak}</span></p>
+            <button class="update-btn">Completed</button>
+            <button class="update-btn">Cancel</button>
+            <button class="update-btn">Delete</button>
         </div>
         <p class="active">active</p>
         `
         grid.appendChild(div)
     });
-    
+
 })();
 
 
