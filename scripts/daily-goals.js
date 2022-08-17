fa345567b935974ca21b8c73ea5d5e14077a5036
edisemin <edisemin@hotmail.com>
2022-08-17 14:40:32 +0100

    Changing logout styling

--- scripts/daily-goals.js
+++ scripts/daily-goals.js
@@ -1,4 +1,30 @@
-import { checkUser } from "./utils.js";
+import { checkUser, logout, getGoals} from "./utils.js";
 checkUser();
+logout();
+
+(async () => {
+    const habits = await getGoals()
+    console.log(habits)
+    const grid = document.querySelector(".goals-grid")
+    
+    habits.forEach(habit => {
+
+        const div = document.createElement('div')
+        div.classList.add("goals-item")
+        div.innerHTML = ` 
+        <img src="images/goals-water.jpg" alt="">
+        <div class='stats-wrapper'> <h2>${habit.habit}</h2>
+            <p><strong>Frequency: </strong>  <span>${habit.frequency}</span></p>
+            <p><strong>Current streak: </strong>  <span>${habit.streak}</span></p>
+        </div>
+        <p class="active">active</p>
+        `
+        grid.appendChild(div)
+    });
+    
+})();
+
+
+
 
 console.log("hello")
