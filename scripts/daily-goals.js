251adfe92a9606759c378445db5b47bf1ea81707
edisemin <edisemin@hotmail.com>
2022-08-18 10:23:51 +0100

    Reafctoring daily-goals file

--- scripts/daily-goals.js
+++ scripts/daily-goals.js
@@ -37,7 +37,7 @@ const completedHabit = (e) => {
     const div = document.createElement("div");
     div.classList.add("goals-item");
     div.innerHTML = ` 
-        <div class="goals-image"> <img src="images/daily-goals-${habit.habit}.jpg" alt=""></div>
+        <div class="goals-image"> <img src="images/daily-goals-${habit.habit.toLowerCase()}.jpg" alt=""></div>
         <div class="goals-item-extra"></div>
         <div class='stats-wrapper'> <h2>${habit.habit}</h2>
             <p><strong>Frequency: </strong>  <span>${habit.frequency}</span></p>
