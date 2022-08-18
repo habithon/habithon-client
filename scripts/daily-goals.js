a7160cff94e6ca01f7950216f2afb7a427399ecb
edisemin <edisemin@hotmail.com>
2022-08-18 10:06:46 +0100

    Refactoring completedHabit method and styling

--- scripts/daily-goals.js
+++ scripts/daily-goals.js
@@ -37,7 +37,8 @@ const completedHabit = (e) => {
     const div = document.createElement("div");
     div.classList.add("goals-item");
     div.innerHTML = ` 
-        <img src="images/goals-water.jpg" alt="">
+        <div class="goals-image"> <img src="images/daily-goals-${habit.habit}.jpg" alt=""></div>
+        <div class="goals-item-extra"></div>
         <div class='stats-wrapper'> <h2>${habit.habit}</h2>
             <p><strong>Frequency: </strong>  <span>${habit.frequency}</span></p>
             <p><strong>Current streak: </strong>  <span>${habit.streak}</span></p>
